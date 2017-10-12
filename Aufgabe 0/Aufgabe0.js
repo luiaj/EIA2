4d0eccc98d37dd5446b0d8fe1bf74a4496e235ef
User <User@192.168.0.46>
2017-10-12 21:15:01 +0200

    Aufgabe 0

---
+++ Aufgabe 0/Aufgabe0.js
@@ -0,0 +1,16 @@
+/*
+Aufgabe: 0, Miniseite
+Name: Julia Kaiser
+Matrikel: 256580
+Datum: 12.10.2017
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+window.addEventListener("load", init);
+function init() {
+    let name = prompt("Hier Ihren Namen eintragen:");
+    if (name != null) {
+        alert("Guten Tag, " + name + "! :)");
+    }
+}
+//# sourceMappingURL=Aufgabe0.js.map
---
+++ Aufgabe 0/Aufgabe0.ts
@@ -0,0 +1,17 @@
+/*
+Aufgabe: 0, Miniseite
+Name: Julia Kaiser
+Matrikel: 256580
+Datum: 12.10.2017
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
+window.addEventListener("load", init);
+
+function init(): void {
+    let name: string = prompt("Hier Ihren Namen eintragen:");
+    if (name != null) {
+          alert("Guten Tag, " + name + "! :)");
+    }
+}
