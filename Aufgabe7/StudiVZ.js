12d58058e265b2bc2d8f7bfa4c300075b1e3bc1e
User <User@192.168.0.45>
2017-12-01 11:39:28 +0100

    Aufgabe 7 und Steckbrief

---
+++ Aufgabe7/StudiVZ.js
@@ -0,0 +1,61 @@
+/*
+Aufgabe: , 7 StudiVZ
+Name: Julia Kaiser
+Matrikel: 256580
+Datum: 01.12.2017
+
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+
+*/
+var StudiVZ;
+(function (StudiVZ) {
+    var students = [];
+    var stop = false;
+    while (!stop) {
+        var action = prompt("Datensatz anlegen (n), abfragen(a) oder Programm beenden (s)\nn,a oder s eingeben");
+        switch (action) {
+            case "n":
+            case "N":
+                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 (M) oder 1(F)) und Kommentar");
+                alert(saveData(input));
+                break;
+            case "a":
+            case "A":
+                var matrikel = parseInt(prompt("Eingabe Matrikelnummer"));
+                alert(queryData(matrikel));
+                break;
+            case "s":
+            case "S":
+                stop = true;
+        }
+    }
+    function saveData(_input) {
+        let splitted = _input.split(",");
+        if (parseInt(splitted[0]) == NaN) {
+            return "Matrikelnummer keine Nummer";
+        }
+        var geschlecht = parseInt(splitted[4]) == 1; //(splitted[4] === "true");
+        let student = {
+            Matrikelnummer: parseInt(splitted[0]),
+            Name: splitted[1],
+            Vorname: splitted[2],
+            Alter: parseInt(splitted[3]),
+            Geschlecht: geschlecht,
+            Kommentar: splitted[5]
+        };
+        students.push(student);
+        return "Eingegebene Daten: " + "\nMatrikelnummer: " + student.Matrikelnummer + "\nName: " + student.Name + "\nVorname: " + student.Vorname + "\nAlter: " + student.Alter + "\nGeschlecht: " + student.Geschlecht + "\nKommentar: " + student.Kommentar;
+    }
+    function queryData(_matrikel) {
+        for (let i = 0; i < students.length; i++) {
+            if (students[i].Matrikelnummer == _matrikel) {
+                return "Student: " + "\nMatrikelnummer: " + students[i].Matrikelnummer + "\nName: " + students[i].Name + "\nVorname: " + students[i].Vorname + "\nAlter: " + students[i].Alter + "\nGeschlecht: " + students[i].Geschlecht + "\nKommentar: " + students[i].Kommentar;
+            }
+            else {
+                return "Matrikelnummer nicht gespeichert";
+            }
+        }
+        return;
+    }
+})(StudiVZ || (StudiVZ = {}));
+//# sourceMappingURL=StudiVZ.js.map
---
+++ Aufgabe7/StudiVZ.ts
@@ -0,0 +1,86 @@
+/*
+Aufgabe: , 7 StudiVZ
+Name: Julia Kaiser
+Matrikel: 256580
+Datum: 01.12.2017
+
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+
+*/
+
+namespace StudiVZ {
+    interface StudentData {
+        Matrikelnummer: number;
+        Name: string;
+        Vorname: string;
+        Alter: number;
+        Geschlecht: boolean;
+        Kommentar: string;
+    }
+
+    var students: StudentData[] = [];
+    var stop: boolean = false;
+
+    while (!stop) {
+        var action: string = prompt("Datensatz anlegen (n), abfragen(a) oder Programm beenden (s)\nn,a oder s eingeben");
+
+        switch (action) {
+            case "n":
+            case "N":
+                var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 (M) oder 1(F)) und Kommentar");
+                alert(saveData(input));
+                break;
+            case "a":
+            case "A":
+                var matrikel: number = parseInt(prompt("Eingabe Matrikelnummer"));
+                alert(queryData(matrikel));
+                break;
+            case "s":
+            case "S":
+                stop = true;
+        }
+    }
+
+    function saveData(_input: string): string {
+        let splitted: string[] = _input.split(",");
+
+        if (parseInt(splitted[0]) == NaN) {
+            return "Matrikelnummer keine Nummer";
+        }
+        
+        
+        var geschlecht: boolean = parseInt(splitted[4]) == 1;   //(splitted[4] === "true");
+
+
+        let student: StudentData = {
+            Matrikelnummer: parseInt(splitted[0]),
+            Name: splitted[1],
+            Vorname: splitted[2],
+            Alter: parseInt(splitted[3]),
+            Geschlecht: geschlecht,
+            Kommentar: splitted[5]
+        };
+
+        students.push(student);
+
+
+        return "Eingegebene Daten: " + "\nMatrikelnummer: " + student.Matrikelnummer + "\nName: " + student.Name + "\nVorname: " + student.Vorname + "\nAlter: " + student.Alter + "\nGeschlecht: " + student.Geschlecht + "\nKommentar: " + student.Kommentar;
+    }
+    
+    
+    function queryData(_matrikel: number): string {
+        
+        for (let i: number = 0; i < students.length; i++) {
+            
+            if (students[i].Matrikelnummer == _matrikel) {
+                return "Student: " + "\nMatrikelnummer: " + students[i].Matrikelnummer + "\nName: " + students[i].Name + "\nVorname: " + students[i].Vorname + "\nAlter: " + students[i].Alter + "\nGeschlecht: " + students[i].Geschlecht + "\nKommentar: " + students[i].Kommentar;
+            }
+            
+                else {
+                return "Matrikelnummer nicht gespeichert";
+                }
+        }
+
+        return;
+    }
+}
